9430873764dc7b830ea27324eaa2e8818d2eced5
Faust <Faustvii@users.noreply.github.com>
2022-08-30 23:32:46 +0200

    ci: have renovate bump app version again

---
+++ tools/bumpAppVersion.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# set -eu
+
+# $1 path to chart if one chart only
+# $2 new version
+
+type=${1}
+chart=${2}
+version=${3}
+
+echo "chart "${type}" "${chart}" is being updated to "${version}""
+
+if [ -d "${chart}" ]; then
+    if [ -z "$version" ];
+    then
+        echo "empty version"
+    else
+        sed -i "s|^appVersion:.*|appVersion: \"${version}\"|g" ${chart}/Chart.yaml
+  fi
+fi
