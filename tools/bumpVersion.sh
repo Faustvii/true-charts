9b514df4117cb0b77b24ce0e9a8a18a4d6a90aa9
Faust <Faustvii@users.noreply.github.com>
2022-08-30 23:57:07 +0200

    ci: try to fix error

--- tools/bumpVersion.sh
+++ tools/bumpVersion.sh
@@ -21,6 +21,7 @@ function incr_semver() {
     patch=${ver[2]}
     minor=${ver[1]}
     major=${ver[0]}
+    echo "$major.$minor.$patch"
 
     case $level in
         patch)
@@ -41,7 +42,8 @@ function incr_semver() {
     esac
     echo "$major.$minor.$patch"
 }
-echo "chart "${type}" "${chart}" is being updated to version "${version}""
+
+echo "chart "${type}" "${chart}" is being updated to version '${version}'"
 
 if [ -d "${chart}" ]; then
     if [ -z "$version" ];
@@ -49,7 +51,9 @@ if [ -d "${chart}" ]; then
         echo "empty version"
     else
         version=$(incr_semver "${version}" "${type}")
+        echo "ver: "${version}""
         DIR="${chart%/*}"/"${version}"
+        echo ${DIR}
         mv -v "$chart" "${DIR}"
         sed -i "s|^version:.*|version: ${version}|g" "${DIR}"/Chart.yaml
         cat "${DIR}"/Chart.yaml | grep "^appVersion:"
